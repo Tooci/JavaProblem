db0b7b07497b818f1818c9f55ccdb83d0c14ad0a
Tooci <844672074@qq.com>
2021-05-20 12:21:47 +0800

    提交

--- src/main/java/p1.java
+++ src/main/java/p1.java
@@ -1,6 +1,11 @@
 public class p1 {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
+       int i=1;
+       i=i++;
+       int j=i++;
+       int k=i+ ++i * i++;
+       System.out.println("i="+i);//i=4
+       System.out.println("j="+j);//j=1
+       System.out.println("k="+k);//k=11
     }
 }
